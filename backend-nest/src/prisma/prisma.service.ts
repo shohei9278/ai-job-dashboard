46daf4ee86e145a4777282a5f67cbefbc374079a
shohei nakahara <sn920708@gmail.com>
2025-10-11 12:17:30 +0900

    不要なログの削除

--- backend-nest/src/prisma/prisma.service.ts
+++ backend-nest/src/prisma/prisma.service.ts
@@ -3,9 +3,7 @@ import { PrismaClient } from '../../generated/prisma';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-  
   async onModuleInit() {
-    console.log('DATABASE_URL:', process.env.DATABASE_URL?.slice(0, 50) + '...');
     await this.$connect();
   }
   async onModuleDestroy() {
