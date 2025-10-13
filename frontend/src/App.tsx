11b55dc900b26d6b83cf4d7f456d39b81ca41194
shohei nakahara <sn920708@gmail.com>
2025-10-14 01:15:03 +0900

    不要な記述削除

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
 import JobsList from "./pages/JobsList";
-import Header from "./components/common/Header";
 import Layout from "./components/layout/Layout";
 
 export default function App() {
   return (
    
     <Router>
-
       <Routes>
         <Route path="/" element={<Layout />} />
         <Route path="/jobs" element={<JobsList />} />
